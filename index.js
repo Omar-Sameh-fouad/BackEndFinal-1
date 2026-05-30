095e2e35bb60dd381c849d77c67150341e918e5b
Omar-Sameh-fouad <omarsamehfouad1@gmail.com>
2026-05-30 20:19:08 +0300

    Test n8n v1

--- index.js
+++ index.js
@@ -9,7 +9,18 @@ const app = express();
 // ================= SECURITY MIDDLEWARES =================
 app.set('trust proxy', 1); 
 app.use(helmet()); 
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5000' }));
+//app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5000' }));
+app.use(cors({
+  origin: [
+    'http://localhost:5000',
+    'http://localhost:5173',
+    'https://final-project-pharma-care.vercel.app',
+    process.env.FRONTEND_URL,
+  ].filter(Boolean),
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 //app.use(cors({ origin: '*' })); 
 app.use(express.json());
 
